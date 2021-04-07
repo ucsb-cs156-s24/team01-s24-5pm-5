3b51d7a761e403165a49add11fd80a554e6b3c34
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-04-06 17:33:52 -0700

    pc - remove unneeded MockBean import

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/services/EarthquakeQueryServiceTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/services/EarthquakeQueryServiceTests.java
@@ -1,19 +1,15 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
-
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
-
 
 @RestClientTest(EarthquakeQueryService.class)
 public class EarthquakeQueryServiceTests {
