9ae2c147566cfa90287cfa504df88e06860f1598
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-01-17 11:16:04 -0800

    pc - change starter code and test for home controller to match w22 team

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeControllerTests.java
@@ -1,8 +1,16 @@
 package edu.ucsb.cs156.spring.backenddemo.controllers;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -24,10 +32,39 @@ public class HomeControllerTests {
     private MockMvc mvc;
 
     @Test
-    public void getHome() throws Exception {
+    public void test_getHome() throws Exception {
         String expectedBody = HomeController.getHomePageObjectJSON("http://localhost/");
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
                 .andExpect(content().string(equalTo(expectedBody)));
+    }
+
+    @Test
+    public void test_getHomePageObjectJSON() throws Exception {
+
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("greeting","Greetings from Spring Boot!");
+
+        String baseUrl = "http://localhost:8080/";
+
+        List<String> team = new ArrayList<String>();
+        team.add("Andrew L.");
+        team.add("Bryan T.");
+        team.add("Bryan Z.");
+        team.add("Kevin H.");
+        team.add("Seth V.");
+        team.add("Phill C.");
+        team.add("Pranav M.");
+        resultMap.put("team",team);
+        resultMap.put("repo","https://github.com/ucsb-cs156-w22/STARTER-team01");
+        resultMap.put("api-documentation", baseUrl + "swagger-ui/");
+        resultMap.put("actuator", baseUrl + "actuator/");
+        String expected = mapper.writeValueAsString(resultMap);
+        String actual = HomeController.getHomePageObjectJSON("http://localhost:8080/");
+
+        assertEquals(expected, actual);
     }
     
 }
