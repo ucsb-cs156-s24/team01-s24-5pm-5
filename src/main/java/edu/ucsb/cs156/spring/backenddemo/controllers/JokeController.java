13f1a86bfc06982bdf20bce758899a596f792056
Phillip Conrad <pconrad@cs.ucsb.edu>
2023-10-17 12:34:54 -0700

    pc - update for f23

---
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/JokeController.java
@@ -0,0 +1,8 @@
+package edu.ucsb.cs156.spring.backenddemo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JokeController {
+    
+}
---
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/JokeQueryService.java
@@ -0,0 +1,23 @@
+package edu.ucsb.cs156.spring.backenddemo.services;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+public class JokeQueryService {
+
+    private final RestTemplate restTemplate;
+
+    public JokeQueryService(RestTemplateBuilder restTemplateBuilder) {
+        restTemplate = restTemplateBuilder.build();
+    }
+
+    public static final String ENDPOINT = "";
+
+    public String getJSON(String location) throws HttpClientErrorException {
+        return "";
+    }
+}
