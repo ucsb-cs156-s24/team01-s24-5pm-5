47f33f4e9200bc922a5075d01f0af704f6b4da63
Gen Tamada <gtamada@ucsb.edu>
2024-04-16 18:08:48 -0700

    Gen - added imports and map for endpoint

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
@@ -1,12 +1,32 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
 
+import java.util.List;
+import java.util.Map;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+// package edu.ucsb.cs156.spring.backenddemo.services;
+
+// import lombok.extern.slf4j.Slf4j;
+
+// import org.springframework.boot.web.client.RestTemplateBuilder;
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.client.HttpClientErrorException;
+// import org.springframework.web.client.RestTemplate;
+
+@Slf4j
 @Service
 public class LocationQueryService {
 
@@ -19,6 +39,8 @@ public class LocationQueryService {
     public static final String ENDPOINT = "https://nominatim.openstreetmap.org/search/search.php?q={location}&format=jsonv2";
 
     public String getJSON(String location) throws HttpClientErrorException {
+        log.info("location={}", location);
+        Map<String, String> uriVariables = Map.of("location", location);
         return "";
     }
 }
