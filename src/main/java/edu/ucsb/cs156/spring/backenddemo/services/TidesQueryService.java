cf3b469292258e606f5b0750b2d41ecf44a433e3
Tejas Vissapragada <tejas.vissa@gmail.com>
2024-04-16 18:05:05 -0700

    tv - define ENDPOINT for tide service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/TidesQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/TidesQueryService.java
@@ -25,7 +25,7 @@ public class TidesQueryService {
     }
 
     // Documentation for endpoint is at: https://api.tidesandcurrents.noaa.gov/api/prod/
-    public static final String ENDPOINT = "";
+    public static final String ENDPOINT = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?application=ucsb-cs156&begin_date={beginDate}&end_date={endDate}&station={station}&product=predictions&datum=mllw&units=english&time_zone=lst_ldt&interval=hilo&format=json";
 
     public String getJSON(String beginDate, String endDate, String station) throws HttpClientErrorException {
         return "";
