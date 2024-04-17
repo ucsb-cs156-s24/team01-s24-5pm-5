0445a7d0da5f66ece2f996b8752a695fc7619f32
Gen Tamada <gtamada@ucsb.edu>
2024-04-16 17:56:56 -0700

    Gen - define endpoint for location query servce

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
@@ -16,7 +16,7 @@ public class LocationQueryService {
         restTemplate = restTemplateBuilder.build();
     }
 
-    public static final String ENDPOINT = "";
+    public static final String ENDPOINT = "https://nominatim.openstreetmap.org/search/search.php?q={location}&format=jsonv2";
 
     public String getJSON(String location) throws HttpClientErrorException {
         return "";
