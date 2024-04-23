202b42f0441871b1df8660f329e4b55553160781
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2024-04-22 18:56:48 -0700

    TS - put back Gen's work

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
@@ -1,24 +1,31 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
-
-
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.client.RestTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
-
-
+// package edu.ucsb.cs156.spring.backenddemo.services;
+// import lombok.extern.slf4j.Slf4j;
+// import org.springframework.boot.web.client.RestTemplateBuilder;
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.client.HttpClientErrorException;
+// import org.springframework.web.client.RestTemplate;
+@Slf4j
 @Service
 public class LocationQueryService {
-
-    ObjectMapper mapper = new ObjectMapper();
-
     private final RestTemplate restTemplate;
-
     public LocationQueryService(RestTemplateBuilder restTemplateBuilder) {
         restTemplate = restTemplateBuilder.build();
     }
-
-    public static final String ENDPOINT = "https://nominatim.openstreetmap.org/search?q={location}&format=jsonv2";
+    public static final String ENDPOINT = "https://nominatim.openstreetmap.org/search/search.php?q={location}&format=jsonv2";
 
     public String getJSON(String location) throws HttpClientErrorException {
         log.info("location={}", location);
@@ -28,7 +35,7 @@ public class LocationQueryService {
 
         Map<String, String> uriVariables = Map.of("location", location);
 
-        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
 
         ResponseEntity<String> re = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class,
                 uriVariables);
